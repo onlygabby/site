b64ae84e72ee58b48e3b66c4daa4d08a61dad9ad
onlygabby <137715879+onlygabby@users.noreply.github.com>
2023-07-13 02:54:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostra_mail() {
+    document.write("mail: <a href=\"mailto:liceopeanopellico&#64;gmail&#46;com\">liceopeanopellico&#64;gmail&#46;com</a> - pec: <a href=\"mailto:cnps02000n&#64;pec&#46;istruzione&#46;it\">cnps02000n&#64;pec&#46;istruzione&#46;it</a>");
+}
